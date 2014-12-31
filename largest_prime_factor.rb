c155654089d9e99abe3500eb123264ba68911ece
smturek <smturek@gmail.com>
2014-12-31 00:00:20 -0700

    problem 3

--- largest_prime_factor.rb
+++ largest_prime_factor.rb
@@ -2,10 +2,15 @@
 
 #What is the largest prime factor of the number 600851475143 ?
 
-def largest_prime_factor
-  result = 0
+require 'Prime'
 
-  
+def largest_prime_factor(number)
+  result = []
+  primes = Prime.first 100
+
+
 
   result
 end
+
+puts largest_prime_factor(600851475143)
