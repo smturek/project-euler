b601642053e18696a95b3a3445d75fc00b625c3a
smturek <smturek@gmail.com>
2014-12-31 21:39:22 -0700

    problem 4 solution

--- palindrome_product.rb
+++ palindrome_product.rb
@@ -4,11 +4,22 @@
 #Find the largest palindrome made from the product of two 3-digit numbers.
 
 def palindrome
-  result = 9009
+  result = 0
+  variables = (100..999).to_a
+  factors = (100..999).to_a
+  product = 0
 
-  if result.to_s == result.to_s.reverse
-    true
+
+  factors.each do |factor|
+    variables.each do |variable|
+      product = factor * variable
+      if product.to_s == product.to_s.reverse && result < product
+        result = product
+      end
+    end
   end
+
+  result
 end
 
 puts palindrome()
