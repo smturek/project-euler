b669c024f0dbdaea1cc51267bbd815bd1c76ed2d
smturek <smturek@gmail.com>
2015-01-03 11:54:22 -0700

    solution to 7

--- 10001st_prime.rb
+++ 10001st_prime.rb
@@ -2,9 +2,20 @@
 #we can see that the 6th prime is 13.
 
 #What is the 10,001st prime number?
+require 'Prime'
 
-def which_prime(number)
+
+def which_prime
   result = 0
+  nth_prime = 0
+  while nth_prime < 10001
+    result += 1
+    if result.prime?
+      nth_prime += 1
+    end
+  end
 
   result
 end
+
+puts which_prime()
